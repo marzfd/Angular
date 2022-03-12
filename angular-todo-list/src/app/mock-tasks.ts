dd3498d0951d76573fbd12a4e60807db7d99c022
Marzieh <mar.31069@gmail.com>
2022-03-12 08:35:32 +0330

    Add mock tasks and tasks componencts

---
+++ angular-todo-list/src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    title: 'Task 1',
+    description: 'Description 1',
+    completed: false
+  },
+  {
+    id: 2,
+    title: 'Task 2',
+    description: 'Description 2',
+    completed: false
+  },
+  {
+    id: 3,
+    title: 'Task 3',
+    description: 'Description 3',
+    completed: true
+  },
+];
